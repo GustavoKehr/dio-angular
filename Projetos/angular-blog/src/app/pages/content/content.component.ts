a0a3ec3603db505295fccf0cd7b357444193a2d8
Gustavo <106829052+GustavoKehr@users.noreply.github.com>
2023-09-28 02:44:58 -0300

    projeto concluido

--- Projetos/angular-blog/src/app/pages/content/content.component.ts
+++ Projetos/angular-blog/src/app/pages/content/content.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { dataFake } from '../../data/dataFake';
 
 @Component({
   selector: 'app-content',
@@ -7,17 +8,25 @@ import { ActivatedRoute } from '@angular/router';
   styleUrls: ['./content.component.css'],
 })
 export class ContentComponent implements OnInit {
-  photoCover: string = 'https://s2-techtudo.glbimg.com/vEfKioHLbZWZ75RhWIOI3ZYwJYU=/0x0:1200x675/984x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_08fbf48bc0524877943fe86e43087e7a/internal_photos/bs/2023/w/x/0XWQNqTNmcs5u1mrS2AQ/counter-strike-2-valve-acesso-beta-teste-global-offensive-csgo-tutorial.jpg';
-  contentTitle: string = 'Minha NOTICIA';
-  contentDescription: string = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia cum nonvel eveniet odit eaque voluptas sequi ipsam! Quia asperiores ipsum iur';
+  photoCover: string = '';
+  contentTitle: string = '';
+  contentDescription: string = '';
+  photoInfo: string = "https://p2.trrsf.com/image/fget/cf/774/0/images.terra.com/2023/03/08/1-1hvbk0li8mx5t.jpeg";
+  private id: string | null = '0';
 
-  constructor(
-    private route:ActivatedRoute
-  ) {}
+  constructor(private route: ActivatedRoute) {}
 
   ngOnInit(): void {
-    this.route.paramMap.subscribe( value =>
-      console.log(value.get("id"))
-    )
+    this.route.paramMap.subscribe((value) => (this.id = value.get('id')));
+
+    this.setValuesToComponent(this.id);
+  }
+
+  setValuesToComponent(id: string | null) {
+    const result = dataFake.filter((article) => article.id == id)[0];
+
+    this.contentTitle = result.title;
+    this.contentDescription = result.description;
+    this.photoCover = result.photoCover;
   }
 }
